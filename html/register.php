2b4a609fee7cac301f5289dc108e2d190380c159
fiddlemyriddle <94159483+fiddlemyriddle@users.noreply.github.com>
2025-03-06 15:17:48 +0800

    add back regvalidate

--- html/register.php
+++ html/register.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/main.css">
     <script type="text/javascript" src="/js/checkStatus.js"></script>
+    <script type="text/javascript" src="/js/regValidate.js"></script>
 </head>
 
 <body>
