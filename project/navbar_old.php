ad3491824e1a64548a14e75c2cf636c2982289cd
lewlipop <156882661+lewlipop@users.noreply.github.com>
2025-03-09 00:32:00 +0800

    Delete "Project" Folder

--- project/navbar_old.php
+++
@@ -1,9 +0,0 @@
-<div id="navbar">
-    <!-- If user is not logged in, show login button -->
-    <a href="./login.php"><button class="navbar-butt">Login</button></a>
-
-    <!-- If user is logged in, replace login button with Log Out button -->
-    <!-- <a href="/login"><button class="navbar-butt">Log Out</button></a> -->
-
-    <a href="./index.php"><button class="navbar-butt">Home</button></a>
-</div>
